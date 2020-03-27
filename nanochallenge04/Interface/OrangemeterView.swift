4693c9ad7df2e180fe95eeacb82e384d2fa16db8
Rodolfo <rodolfobiazi@hotmail.com>
2020-03-26 22:11:12 -0300

    Orangemeter quase pronto

--- nanochallenge04/Interface/OrangemeterView.swift
+++ nanochallenge04/Interface/OrangemeterView.swift
@@ -11,16 +11,42 @@ import SwiftUI
 struct OrangemeterView: View {
     var body: some View {
         GeometryReader { goma in
-        ZStack {
-        Image("OrangemeterFull")
-        .resizable()
-        .scaledToFit()
-            .frame(width: goma.size.width/4)
-        Image("Goma1")
-        .resizable()
-        .scaledToFit()
-            .frame(width: goma.size.width/12)
-            
+            ZStack {
+                Image("OrangemeterFull")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/4)
+                    .opacity(0.5)
+                Image("Goma1")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/12)
+                    .offset(x: 0.045 * goma.size.width, y: -0.06 * goma.size.width)
+                Image("Goma2")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/12)
+                    .offset(x: 0.0625 * goma.size.width)
+                Image("Goma3")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/12.7)
+                    .offset(x: 0.042 * goma.size.width, y: 0.06 * goma.size.width)
+                Image("Goma4")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/13.1)
+                    .offset(x: -0.046 * goma.size.width, y: 0.06 * goma.size.width)
+                Image("Goma5")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/12)
+                    .offset(x: -0.0625 * goma.size.width)
+                Image("Goma6")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: goma.size.width/12)
+                    .offset(x: -0.045 * goma.size.width, y: -0.06 * goma.size.width)
             }
         }
     }
