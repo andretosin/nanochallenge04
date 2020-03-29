623057609329c0003f8eef7322578d52c816417d
André Tosin <andre_tosin@hotmail.com>
2020-03-28 21:37:55 -0300

    Troca o asset do tunel

--- nanochallenge04/Model/Orange.swift
+++ nanochallenge04/Model/Orange.swift
@@ -13,13 +13,13 @@ class Orange: Spawnable {
     var lastTime: TimeInterval = TimeInterval(0)
     var timeInterval: Double = Double.random(in: Double(2)...Double(10))
     var scene: SKScene?
-    let fullTexture = SKTexture(imageNamed: "OrangeFull")
+    let fullTexture = SKTexture(imageNamed: "OneRing")
     var isSpawnActive = false
     var orangeArray: [SKSpriteNode] = []
     var speed = CGFloat(1000)
     var resetFlagFunc: (() -> Void)!
     
-    let fullNode: SKSpriteNode = SKSpriteNode(imageNamed: "OrangeFull")
+    let fullNode: SKSpriteNode = SKSpriteNode(imageNamed: "OneRing")
     
     internal init(scene: SKScene?, resetFlag: @escaping () -> Void) {
         self.scene = scene
@@ -27,7 +27,7 @@ class Orange: Spawnable {
         
         setupOrange(fullNode: fullNode, x: -1000, y: 0, speed: speed)
         
-        self.fullNode.scale(to: CGSize(width: 420*0.7, height: 424*0.7))
+//        self.fullNode.scale(to: CGSize(width: 420*0.7, height: 424*0.7))
 
         
         
@@ -42,14 +42,14 @@ class Orange: Spawnable {
     
     func setupOrange(fullNode: SKSpriteNode, x: CGFloat, y: CGFloat, speed: CGFloat) {
         
+        let collisionMask = SKTexture(imageNamed: "starCollisionMask")
         fullNode.position = CGPoint(x: x, y: y)
         fullNode.zPosition = 4
 //        fullNode.scale(to: CGSize(width: 200, height: 200))
 
 
         
-        fullNode.physicsBody = SKPhysicsBody(texture: fullNode
-            .texture!, size: fullNode.texture!.size())
+        fullNode.physicsBody = SKPhysicsBody(texture: collisionMask, size: collisionMask.size())
         fullNode.physicsBody?.categoryBitMask = ContactMask.orange.rawValue
         fullNode.physicsBody?.contactTestBitMask = ContactMask.player.rawValue
         fullNode.physicsBody?.collisionBitMask = 0
