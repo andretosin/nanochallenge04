5e61dac8138aa115f1d35f44c22f38922e9751ec
Rodolfo <rodolfobiazi@hotmail.com>
2020-03-31 14:26:47 -0300

    PopUp UI

--- nanochallenge04/Interface/PopUpView.swift
+++ nanochallenge04/Interface/PopUpView.swift
@@ -12,32 +12,41 @@ struct PopUpView: View {
     @Binding var showPopup: Bool
     var body: some View {
         ZStack {
-//            if self.showPopup {
-                Color.black
-                    .opacity(0.4)
-                    .edgesIgnoringSafeArea(.all)
-                    .onTapGesture { self.showPopup = false }
-                VStack(spacing: 20) {
-                    Text("Oops... our bad")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.yellow)
-                    Spacer()
-                    Text("""
+            //            if self.showPopup {
+            Color.black
+                .opacity(0.4)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture { self.showPopup = false }
+            VStack(spacing: 20) {
+                Text("Oops... our bad")
+                    .font(.custom("nulshock", size: 18))
+                    .foregroundColor(Color("CosmicPurple"))
+                    .bold()
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.yellow)
+                Spacer()
+                Text("""
 We are still working on this feature!
 Come back later
 """)
-                    Spacer()
-                    Button("Close") {
-                        self.showPopup = false
-                    }.padding(.bottom)
-                }
-                .frame(height: 300)
-                .background(Color.white)
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                .padding(.horizontal, 25)
-//            }
+//                    .font(.custom("nulshock", size: 18))
+                    .foregroundColor(Color("CosmicPurple"))
+                    .bold()
+                    .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                
+                Spacer()
+                Button("Close") {
+                    self.showPopup = false
+                }.padding(.bottom)
+            }
+            .frame(height: 300)
+            .background(Color.white)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .padding(.horizontal, 25)
+            //            }
         }
     }
 }
